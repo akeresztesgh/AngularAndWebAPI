f7be2e88be4f1fda0d8f7e366781ed7d6a750752
AJ Keresztes <akeresztes@ibs.com>
2016-07-18 09:37:21 -0400

    added test to generate audience id and secret

---
+++ api/api.tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace api.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // used for generating audience id and audience secret
+
+        [TestMethod]
+        public void GenerateEncryptedAudienceData()
+        {
+            var audienceId = Guid.NewGuid().ToString("N");
+
+            var key = new byte[32];
+            RNGCryptoServiceProvider.Create().GetBytes(key);
+            var audienceSecret = TextEncodings.Base64Url.Encode(key);
+
+            Debug.WriteLine($"AudienceId: {audienceId}\nAudienceSecret: {audienceSecret}");
+        }
+    } 
+}
